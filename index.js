ea0db3d0f8b8a4bb878896f1bfdc2a79d8067d39
BaselMohab <95124859+BaselMohab@users.noreply.github.com>
2023-11-08 15:34:25 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,40 @@
+const questionEl = document.getElementById("question");
+const formEl = document.getElementById("form");
+const inputEl = document.getElementById("input");
+const scoreEl = document.getElementById("score")
+
+
+const num1 = Math.floor(Math.random() * 10);
+const num2 = Math.floor(Math.random() * 10);
+
+
+questionEl.innerText = `Whats is ${num1} multiply by ${num2} ?`;
+
+const correctAnswer = num1 * num2;
+let score = JSON.parse(localStorage.getItem("score"));
+
+if (!score) {
+    score = 0;
+}
+
+scoreEl.innerText = `score: ${score}`;
+
+
+function checkAnswer() {
+    const Answer = parseInt(inputEl.value)
+    if (Answer === correctAnswer) {
+        score++
+        updateLocalStorage()
+    }else {
+        score--
+        updateLocalStorage()
+    }
+}
+
+
+function updateLocalStorage() {
+    localStorage.setItem('score', JSON.stringify(score));
+}
+
+
+formEl.addEventListener("submit", checkAnswer);
